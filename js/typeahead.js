2e64981e12d7eb8c9940ca7cf8d2e6a779e048b8
sinne24 <cindy.hernandez5224@gmail.com>
2022-12-13 12:59:06 -0700

    added logic to clear suggestions, and only show suggestion that start with the userInput

--- js/typeahead.js
+++ js/typeahead.js
@@ -1,9 +1,19 @@
 
 const arrayOfObjects = [
-    {name: "Fox", link: "foxnews.com"},
-    {name: "Fandango", link: "fandango.com"}, 
-    {name: "CNN", link: "cnn.com"}
-];
+    { name: "Fox", link: "foxnews.com" },
+    { name: "Fandango", link: "fandango.com" },
+    { name: "CNN", link: "cnn.com" },
+    { name: "Google", link: "google.com" },
+    { name: "YouTube", link: "youtube.com" },
+    { name: "Instagram", link: "instagram.com" },
+    { name: "Amazon", link: "amazon.com" },
+    { name: "RideFox", link: "ridefox.com" },
+    { name: "Udemy", link: "udemy.com" },
+    { name: "Telegram", link: "telegram.org" },
+    { name: "Discord", link: "discord.com" },
+    { name: "Slack", link: "slack.com" },
+  
+  ];
 
 const SUGGESTIONS_ID = "suggestions";
 
@@ -12,20 +22,24 @@ searchInput.addEventListener("keyup", handleKeyUp);
 
 function handleKeyUp(e) {
     // console.log(searchInput.value); check that we are checking what the user types
+    const userInput = searchInput.value.toLowerCase();
 
-    const userInput = searchInput.value;
-    //check for words that contain the text the user has typed
-    const suggestions = arrayOfObjects.filter((result) => 
-        result.name.toLowerCase().includes(userInput)
-    );
-    // console.log(suggestions);check that the suggestions are populating correctly   
-    showSuggestions(suggestions);
+    //clear existing selections
+    clearSuggestions();
+
+    //We only want suggestions to show if their is input in the search field
+    if(userInput){
+        //check for words that contain the text the user has typed
+        const suggestions = arrayOfObjects.filter((result) => 
+            result.name.toLowerCase().startsWith(userInput)
+        );
+        // console.log(suggestions);check that the suggestions are populating correctly   
+        showSuggestions(suggestions);
+    }
 }
 
 function showSuggestions(suggestions){
     const ul = document.getElementById(SUGGESTIONS_ID);
-    //clear existing selections
-    clearSuggestions();
 
     //add elements that are applicable
     suggestions.forEach((result) => {
